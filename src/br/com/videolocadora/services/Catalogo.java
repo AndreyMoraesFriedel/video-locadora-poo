b85ec12300bb4313c735290d924ccc044b020f9c
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-08-22 23:26:55 -0300

    Classe Catalogo Construido

---
+++ src/br/com/videolocadora/services/Catalogo.java
@@ -0,0 +1,32 @@
+package br.com.videolocadora.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.videolocadora.models.Filme;
+
+public class Catalogo {
+    private static int contadorId;
+    private final int id;
+    private List<Filme> filmes = new ArrayList<>();
+
+    public Catalogo(){
+        this.id = contadorId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void cadastrarFilme(Filme filme) {
+        filmes.add(filme);
+    }
+
+    public List<Filme> consultarFilmes() {
+        return filmes;
+    }
+
+    public void removerFilme(int id) {
+        filmes.remove(id);
+    }
+}
